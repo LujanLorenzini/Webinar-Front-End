12aa77976d66f208220d27898ed8245a737e2305
Lujan Lorenzini <lujanlorenzini@icloud.com>
2021-02-04 12:56:59 -0300

    webinar Front End- Educacion IT

---
+++ js/codigo.js
@@ -0,0 +1,12 @@
+function reproducir(){
+    document.getElementById('video').play();
+}
+function parar(){
+    document.getElementById('video').pause();
+}
+function video1(){
+    document.getElementById('video') .src= 'videos/video-01.mp4';
+}
+function video2(){
+    document.getElementById('video') .src= 'videos/video-02.mp4';
+}
